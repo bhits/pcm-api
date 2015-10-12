1f1b2f4e379169f62bc703e156dc9a071143f92b
CindyLiFei <cindy.li@feisystems.com>
2015-10-12 16:14:10 -0400

    Disable csrf token
    #7056 - Create Patient Consent Management API

--- pcm/web/src/main/java/gov/samhsa/consent2share/web/config/di/root/WebSecurityConfig.java
+++ pcm/web/src/main/java/gov/samhsa/consent2share/web/config/di/root/WebSecurityConfig.java
@@ -49,8 +49,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf()
-		.and()
+		http.csrf().disable()
 			.headers()
 			.cacheControl()
 			.frameOptions()
