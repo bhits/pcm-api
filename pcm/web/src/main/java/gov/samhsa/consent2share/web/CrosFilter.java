11aae5f80c9df24268b574343bc9d381512c3bc0
CindyLiFei <cindy.li@feisystems.com>
2015-10-12 13:52:12 -0400

    Fix Cross domain filter error
    #7056 - Create Patient Consent Management API

--- pcm/web/src/main/java/gov/samhsa/consent2share/web/CrosFilter.java
+++ pcm/web/src/main/java/gov/samhsa/consent2share/web/CrosFilter.java
@@ -11,27 +11,16 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 public class CrosFilter extends OncePerRequestFilter {
 
-	static final String ORIGIN = "Origin";
-
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
 			throws ServletException, IOException {
-		if (request.getHeader(ORIGIN) == null || request.getHeader(ORIGIN).equals("null")) {
-			response.setHeader("Access-Control-Allow-Origin", "*");
-			response.setHeader("Access-Control-Allow-Credentials", "true");
-			response.setHeader("Access-Control-Allow-Headers",
-			request.getHeader("Access-Control-Request-Headers"));
-		}
-		if (request.getMethod().equals("OPTIONS")) {
-			try {
-				response.getWriter().print("OK");
-				response.getWriter().flush();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		} else {
-			filterChain.doFilter(request, response);
-		}
+
+		response.addHeader("Access-Control-Allow-Origin", "*");
+		response.addHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE");
+		response.addHeader("Access-Control-Max-Age", "3600");
+		response.addHeader("Access-Control-Allow-Headers",
+				"Origin, X-Requested-With, Content-Type, Accept, Authorization");
+		filterChain.doFilter(request, response);
 
 	}
 
