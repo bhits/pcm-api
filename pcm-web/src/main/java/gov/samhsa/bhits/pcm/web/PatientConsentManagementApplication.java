2452ecc3d29caf5247d76eee40e77c64aceface4
burcakulug <burcak.ulug@feisystems.com>
2016-01-18 17:06:47 -0500

    Add initial OAuth security configuration to the new PCM API
    #7802:As a developer, I can apply UAA authorization to the PCM API to make the application secure

--- pcm-web/src/main/java/gov/samhsa/bhits/pcm/web/PatientConsentManagementApplication.java
+++ pcm-web/src/main/java/gov/samhsa/bhits/pcm/web/PatientConsentManagementApplication.java
@@ -5,10 +5,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.orm.jpa.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
 @EntityScan(basePackageClasses = PcmDomainMarkerInterface.class)
 @EnableJpaRepositories(basePackageClasses = PcmDomainMarkerInterface.class)
+@EnableResourceServer
 public class PatientConsentManagementApplication {
 
     public static void main(String[] args) {
