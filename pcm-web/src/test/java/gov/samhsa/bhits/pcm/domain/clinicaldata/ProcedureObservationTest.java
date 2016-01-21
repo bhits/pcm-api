acb32bda2859990b1c27a9a67048133b62244ded
burcakulug <burcak.ulug@feisystems.com>
2016-01-21 13:44:18 -0500

    Copy unit tests for PCM domain
    #7802:As a developer, I can apply UAA authorization to the PCM API to make the application secure

---
+++ pcm-web/src/test/java/gov/samhsa/bhits/pcm/domain/clinicaldata/ProcedureObservationTest.java
@@ -0,0 +1,15 @@
+package gov.samhsa.bhits.pcm.domain.clinicaldata;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.springframework.mock.staticmock.MockStaticEntityMethods;
+
+@RunWith(JUnit4.class)
+@MockStaticEntityMethods
+public class ProcedureObservationTest {
+
+    @Test
+    public void testMethod() {
+    }
+}
