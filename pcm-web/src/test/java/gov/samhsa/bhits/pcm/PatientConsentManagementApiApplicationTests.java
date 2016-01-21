9398eb6832cebe091719951618cb18bcf4f29bcc
burcakulug <burcak.ulug@feisystems.com>
2016-01-21 13:21:56 -0500

    Move SpringBoot application starter test to correct package
    #7802:As a developer, I can apply UAA authorization to the PCM API to make the application secure

--- pcm-web/src/test/java/gov/samhsa/bhits/pcm/PatientConsentManagementApiApplicationTests.java
+++ pcm-web/src/test/java/gov/samhsa/bhits/pcm/PatientConsentManagementApiApplicationTests.java
@@ -1,19 +1,18 @@
-package gov.samhsa.bhits.pcm.web;
+package gov.samhsa.bhits.pcm;
 
-import gov.samhsa.bhits.pcm.PatientConsentManagementApplication;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = PatientConsentManagementApplication.class)
 @WebAppConfiguration
 public class PatientConsentManagementApiApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
