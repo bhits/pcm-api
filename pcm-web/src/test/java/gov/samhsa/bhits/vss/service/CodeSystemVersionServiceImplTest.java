55e0b263ba16f4d147e39bfdcd85ca28815ff81c
burcakulug <burcak.ulug@feisystems.com>
2016-01-22 16:00:15 -0500

    Copy unit tests for value set service
    #7802:As a developer, I can apply UAA authorization to the PCM API to make the application secure

---
+++ pcm-web/src/test/java/gov/samhsa/bhits/vss/service/CodeSystemVersionServiceImplTest.java
@@ -0,0 +1,189 @@
+package gov.samhsa.bhits.vss.service;
+
+import gov.samhsa.bhits.pcm.domain.valueset.*;
+import gov.samhsa.bhits.vss.service.dto.CodeSystemVersionCSDto;
+import gov.samhsa.bhits.vss.service.dto.CodeSystemVersionDto;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.anyLong;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class CodeSystemVersionServiceImplTest {
+
+    @Mock
+    CodeSystemRepository codeSystemRepository;
+
+    @Mock
+    CodeSystemVersionRepository codeSystemVersionRepository;
+
+    @Mock
+    ValueSetMgmtHelper valueSetMgmtHelper;
+    ;
+
+    @InjectMocks
+    CodeSystemVersionServiceImpl vst;
+
+    @Test(expected = CodeSystemNotFoundException.class)
+    public void testCreateCodeSystemVersion_throw_CodeSystemNotFoundException()
+            throws CodeSystemNotFoundException {
+        CodeSystemVersionDto createdMock = mock(CodeSystemVersionDto.class);
+        when(createdMock.getDescription()).thenReturn("description");
+        when(createdMock.getName()).thenReturn("name");
+        when(createdMock.getCode()).thenReturn("code");
+        when(createdMock.getUserName()).thenReturn("username");
+
+        ValueSetCategory selected = new ValueSetCategory();
+        selected.setCode("code");
+        selected.setName("name");
+        selected.setId((long) 1);
+        when(codeSystemRepository.findOne((long) 1)).thenReturn(null);
+
+        vst.create(createdMock);
+    }
+
+    @Test
+    public void testDeleteCodeSystemVersion()
+            throws CodeSystemVersionNotFoundException {
+
+        CodeSystemVersion deleted = mock(CodeSystemVersion.class);
+        when(codeSystemVersionRepository.findOne(anyLong()))
+                .thenReturn(deleted);
+
+        CodeSystemVersionDto codeSystemVersionDto = mock(CodeSystemVersionDto.class);
+        when(valueSetMgmtHelper.createCodeSystemVersionDtoFromEntity(deleted))
+                .thenReturn(codeSystemVersionDto);
+
+        Assert.assertEquals(vst.delete((long) 1), codeSystemVersionDto);
+    }
+
+    @Test(expected = CodeSystemVersionNotFoundException.class)
+    public void testDeleteCodeSystemVersion_throw_CodeSystemVersionNotFoundException()
+            throws CodeSystemVersionNotFoundException {
+
+        when(codeSystemVersionRepository.findOne(anyLong())).thenReturn(null);
+        vst.delete((long) 1);
+
+    }
+
+    @SuppressWarnings({"unchecked", "rawtypes"})
+    @Test
+    public void testfindAll() {
+
+        List<CodeSystemVersion> codeSystemVersionsMock = mock(List.class);
+        when(codeSystemVersionRepository.findAll()).thenReturn(
+                codeSystemVersionsMock);
+
+        List<CodeSystemVersionDto> codeSystemVersionDtos = new ArrayList();
+        CodeSystemVersionDto codeSystemVersionDto = mock(CodeSystemVersionDto.class);
+        codeSystemVersionDtos.add(codeSystemVersionDto);
+        when(
+                valueSetMgmtHelper
+                        .convertCodeSystemVersionEntitiesToDtos(codeSystemVersionsMock))
+                .thenReturn(codeSystemVersionDtos);
+
+        assertEquals(vst.findAll(), codeSystemVersionDtos);
+    }
+
+    @Test
+    public void testfindId() throws CodeSystemNotFoundException {
+
+        CodeSystemVersion codeSystemVersionMock = mock(CodeSystemVersion.class);
+
+        List<CodeSystem> codeSystemsMock = new ArrayList<CodeSystem>();
+        CodeSystem codeSystemMock = mock(CodeSystem.class);
+        codeSystemsMock.add(codeSystemMock);
+
+        CodeSystemVersionDto codeSystemVersionDtoMock = mock(CodeSystemVersionDto.class);
+
+        when(codeSystemVersionRepository.findOne(anyLong())).thenReturn(
+                codeSystemVersionMock);
+        when(codeSystemRepository.findAll()).thenReturn(codeSystemsMock);
+
+        when(
+                valueSetMgmtHelper
+                        .createCodeSystemVersionDtoFromEntity(codeSystemVersionMock))
+                .thenReturn(codeSystemVersionDtoMock);
+
+        assertEquals(vst.findById((long) 1), codeSystemVersionDtoMock);
+    }
+
+    @Test
+    public void testUpdateCodeSystemVersion()
+            throws CodeSystemVersionNotFoundException,
+            CodeSystemNotFoundException {
+
+        CodeSystemVersionDto updatedMock = mock(CodeSystemVersionDto.class);
+
+        when(updatedMock.getId()).thenReturn((long) 1);
+        when(updatedMock.getCode()).thenReturn("code");
+        when(updatedMock.getName()).thenReturn("name");
+        when(updatedMock.getUserName()).thenReturn("username");
+        CodeSystemVersionDto codeSystemVersionDtoMock = mock(CodeSystemVersionDto.class);
+
+        CodeSystemVersion codeSystemVersionMock = mock(CodeSystemVersion.class);
+
+        CodeSystem codeSystemMock = mock(CodeSystem.class);
+
+        when(codeSystemVersionRepository.findOne(anyLong())).thenReturn(
+                codeSystemVersionMock);
+
+        when(codeSystemVersionMock.getCodeSystem()).thenReturn(codeSystemMock);
+
+        when(codeSystemMock.getId()).thenReturn((long) 1);
+
+        when(codeSystemRepository.findOne(anyLong()))
+                .thenReturn(codeSystemMock);
+
+        when(
+                valueSetMgmtHelper
+                        .createCodeSystemVersionDtoFromEntity(codeSystemVersionMock))
+                .thenReturn(codeSystemVersionDtoMock);
+
+        assertEquals(vst.update(updatedMock), codeSystemVersionDtoMock);
+    }
+
+    @Test(expected = CodeSystemVersionNotFoundException.class)
+    public void testUpdateCodeSystemVersion_throw_CodeSystemVersionNotFoundException()
+            throws CodeSystemVersionNotFoundException,
+            CodeSystemNotFoundException {
+        CodeSystemVersionDto updated = mock(CodeSystemVersionDto.class);
+
+        when(codeSystemVersionRepository.findOne(anyLong())).thenReturn(null);
+        vst.update(updated);
+
+    }
+
+    @SuppressWarnings("unchecked")
+    @Test
+    public void testCodeSystemVersionCSDto() throws CodeSystemNotFoundException {
+        CodeSystemVersionCSDto codeSystemVersionVSCDto = new CodeSystemVersionCSDto();
+
+        List<CodeSystem> codeSystemsMock = new ArrayList<CodeSystem>();
+        CodeSystem codeSystem = mock(CodeSystem.class);
+        codeSystemsMock.add(codeSystem);
+
+        when(codeSystemRepository.findAll()).thenReturn(codeSystemsMock);
+
+        Map<Long, String> codeSystemCategoriesMap = mock(Map.class);
+
+        when(valueSetMgmtHelper.convertCodeSystemEntitiesToMap(codeSystemsMock))
+                .thenReturn(codeSystemCategoriesMap);
+        codeSystemVersionVSCDto.setCodeSystemDtoMap(codeSystemCategoriesMap);
+
+        vst.create();
+
+    }
+
+}
---
+++ pcm-web/src/test/java/gov/samhsa/bhits/vss/service/ConceptCodeServiceImplTest.java
@@ -0,0 +1,395 @@
+package gov.samhsa.bhits.vss.service;
+
+import gov.samhsa.bhits.pcm.domain.valueset.*;
+import gov.samhsa.bhits.vss.service.dto.ConceptCodeDto;
+import gov.samhsa.bhits.vss.service.dto.ConceptCodeVSCSDto;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ConceptCodeServiceImplTest {
+
+    private final int conceptCodePageSize = 20;
+
+    @Mock
+    ConceptCodeRepository conceptCodeRepository;
+
+    @Mock
+    ValueSetRepository valueSetRepository;
+
+    @Mock
+    CodeSystemRepository codeSystemRepository;
+
+    @Mock
+    CodeSystemVersionRepository codeSystemVersionRepository;
+
+    @Mock
+    ConceptCodeValueSetRepository conceptCodeValueSetRepository;
+
+    @Mock
+    ValueSetMgmtHelper valueSetMgmtHelper;
+
+    @InjectMocks
+    ConceptCodeServiceImpl conceptCodeServiceImpl = new ConceptCodeServiceImpl(
+            conceptCodePageSize, conceptCodeRepository, valueSetRepository,
+            codeSystemRepository, codeSystemVersionRepository,
+            conceptCodeValueSetRepository, valueSetMgmtHelper);
+
+    @Test
+    public void testDeleteConceptCode() throws ConceptCodeNotFoundException {
+
+        ConceptCode deleted = mock(ConceptCode.class);
+        when(conceptCodeRepository.findOne(anyLong())).thenReturn(deleted);
+
+        ConceptCodeDto conceptCodeDto = mock(ConceptCodeDto.class);
+        when(valueSetMgmtHelper.createConceptCodeDtoFromEntity(deleted))
+                .thenReturn(conceptCodeDto);
+
+        Assert.assertEquals(conceptCodeServiceImpl.delete((long) 1),
+                conceptCodeDto);
+    }
+
+    @Test(expected = ConceptCodeNotFoundException.class)
+    public void testDeleteConceptCode_throw_ConceptCodeNotFoundException()
+            throws ConceptCodeNotFoundException {
+
+        when(conceptCodeRepository.findOne(anyLong())).thenReturn(null);
+        conceptCodeServiceImpl.delete((long) 1);
+
+    }
+
+    @Test(expected = ValueSetNotFoundException.class)
+    public void testfindId() throws ValueSetNotFoundException,
+            ConceptCodeNotFoundException {
+
+        ConceptCode conceptCode = mock(ConceptCode.class);
+
+        when(conceptCodeRepository.findOne(anyLong())).thenReturn(conceptCode);
+
+        ConceptCodeDto conceptCodeDto = mock(ConceptCodeDto.class);
+        when(valueSetMgmtHelper.createConceptCodeDtoFromEntity(conceptCode))
+                .thenReturn(conceptCodeDto);
+
+        conceptCodeServiceImpl.findById((long) 1);
+
+    }
+
+    @Test(expected = ValueSetNotFoundException.class)
+    public void testUpdateConceptCode_throw_ValueSetNotFoundException()
+            throws ConceptCodeNotFoundException, ValueSetNotFoundException {
+
+        ConceptCodeDto updated = mock(ConceptCodeDto.class);
+
+        when(updated.getId()).thenReturn((long) 1);
+        when(updated.getCode()).thenReturn("code");
+        when(updated.getName()).thenReturn("name");
+        when(updated.getUserName()).thenReturn("username");
+
+        ConceptCode conceptCode = mock(ConceptCode.class);
+
+        when(conceptCodeRepository.findOne(anyLong())).thenReturn(conceptCode);
+
+        ConceptCodeDto conceptCodeDto = mock(ConceptCodeDto.class);
+        when(valueSetMgmtHelper.createConceptCodeDtoFromEntity(conceptCode))
+                .thenReturn(conceptCodeDto);
+
+        conceptCodeServiceImpl.update(updated);
+    }
+
+    @Test(expected = ConceptCodeNotFoundException.class)
+    public void testUpdateConceptCode_throw_ConceptCodeNotFoundException()
+            throws ConceptCodeNotFoundException, ValueSetNotFoundException {
+        ConceptCodeDto updated = mock(ConceptCodeDto.class);
+        when(conceptCodeRepository.findOne(anyLong())).thenReturn(null);
+        conceptCodeServiceImpl.update(updated);
+
+    }
+
+    @Test
+    public void testUpdateConceptCode2() throws ConceptCodeNotFoundException,
+            ValueSetNotFoundException {
+
+        ConceptCodeDto updated = mock(ConceptCodeDto.class);
+
+        when(updated.getId()).thenReturn((long) 1);
+        when(updated.getCode()).thenReturn("code");
+        when(updated.getName()).thenReturn("name");
+        when(updated.getUserName()).thenReturn("username");
+
+        ConceptCode conceptCode = mock(ConceptCode.class);
+        when(conceptCode.getId()).thenReturn((long) 1);
+
+        when(conceptCodeRepository.findOne(anyLong())).thenReturn(conceptCode);
+
+        ConceptCodeDto conceptCodeDto = mock(ConceptCodeDto.class);
+        when(valueSetMgmtHelper.createConceptCodeDtoFromEntity(conceptCode))
+                .thenReturn(conceptCodeDto);
+
+        List<Long> selVsIds = Arrays.asList((long) 1, (long) 2, (long) 3);
+        when(updated.getValueSetIds()).thenReturn(selVsIds);
+
+        List<ConceptCodeValueSet> cValueSets = new ArrayList();
+        ConceptCodeValueSet aCodeValueSet = mock(ConceptCodeValueSet.class);
+        cValueSets.add(aCodeValueSet);
+
+        when(conceptCodeValueSetRepository.findAllByPkConceptCodeId(anyLong()))
+                .thenReturn(cValueSets);
+        when(aCodeValueSet.getValueSet()).thenReturn(mock(ValueSet.class));
+
+        when(aCodeValueSet.getValueSet().getId()).thenReturn((long) 1);
+
+        when(valueSetMgmtHelper.createConceptCodeDtoFromEntity(conceptCode))
+                .thenReturn(updated);
+
+        // assertEquals(vst.update(updated),updated);
+    }
+
+    @Test(expected = ValueSetNotFoundException.class)
+    public void testUpdateConceptCode_throw_valueSetNotFoundException2()
+            throws ConceptCodeNotFoundException, ValueSetNotFoundException {
+
+        ConceptCodeDto updated = mock(ConceptCodeDto.class);
+
+        when(updated.getId()).thenReturn((long) 1);
+        when(updated.getCode()).thenReturn("code");
+        when(updated.getName()).thenReturn("name");
+        when(updated.getUserName()).thenReturn("username");
+
+        ConceptCode conceptCode = mock(ConceptCode.class);
+        when(conceptCode.getId()).thenReturn((long) 1);
+
+        when(conceptCodeRepository.findOne(anyLong())).thenReturn(conceptCode);
+
+        ConceptCodeDto conceptCodeDto = mock(ConceptCodeDto.class);
+        when(valueSetMgmtHelper.createConceptCodeDtoFromEntity(conceptCode))
+                .thenReturn(conceptCodeDto);
+
+        List<Long> selVsIds = Arrays.asList((long) 1, (long) 2, (long) 3);
+        when(updated.getValueSetIds()).thenReturn(selVsIds);
+
+        when(conceptCodeValueSetRepository.findAllByPkConceptCodeId(anyLong()))
+                .thenReturn(null);
+
+        conceptCodeServiceImpl.update(updated);
+    }
+
+    @Test
+    public void testfindAll() {
+
+        List<ConceptCode> conceptCodes = mock(List.class);
+        when(conceptCodeRepository.findAll()).thenReturn(conceptCodes);
+
+        List<ConceptCodeDto> conceptCodeDtos = new ArrayList();
+        ConceptCodeDto conceptCodeDto = mock(ConceptCodeDto.class);
+        conceptCodeDtos.add(conceptCodeDto);
+        when(valueSetMgmtHelper.convertConceptCodeEntitiesToDtos(conceptCodes))
+                .thenReturn(conceptCodeDtos);
+
+        assertEquals(conceptCodeServiceImpl.findAll(), conceptCodeDtos);
+    }
+
+    @Test
+    public void testFindAllByName() {
+        Page<ConceptCode> conceptCodes = mock(Page.class);
+        List<ConceptCodeDto> conceptCodeDtos = mock(List.class);
+
+        when(
+                conceptCodeRepository.findAllByName(anyString(), anyString(),
+                        anyString(), anyString(), any(Pageable.class)))
+                .thenReturn(conceptCodes);
+        when(
+                valueSetMgmtHelper
+                        .convertConceptCodeEntitiesToDtos(conceptCodes
+                                .getContent())).thenReturn(conceptCodeDtos);
+        assertEquals(
+                conceptCodeServiceImpl.findAllByName("a", null, null, null, 0)
+                        .get("conceptCodes"), conceptCodeDtos);
+
+    }
+
+    @Test
+    public void testFindAllByCode() {
+
+        Page<ConceptCode> conceptCodes = mock(Page.class);
+        List<ConceptCodeDto> conceptCodeDtos = mock(List.class);
+
+        when(
+                conceptCodeRepository.findAllByCodeLike(anyString(),
+                        anyString(), anyString(), anyString(),
+                        any(Pageable.class))).thenReturn(conceptCodes);
+        when(
+                valueSetMgmtHelper
+                        .convertConceptCodeEntitiesToDtos(conceptCodes
+                                .getContent())).thenReturn(conceptCodeDtos);
+        assertEquals(
+                conceptCodeServiceImpl.findAllByCode("a", null, null, null, 0)
+                        .get("conceptCodes"), conceptCodeDtos);
+    }
+
+    @Test(expected = ValueSetNotFoundException.class)
+    public void testCreateCodeSystem_throw_ValueSetCategoryNotFoundException()
+            throws ValueSetNotFoundException,
+            CodeSystemVersionNotFoundException, CodeSystemNotFoundException {
+        ConceptCodeVSCSDto conceptCodeVSCSDto = mock(ConceptCodeVSCSDto.class);
+        List<ValueSet> valueSets = mock(List.class);
+        when(valueSetRepository.findAll()).thenReturn(valueSets);
+        ConceptCodeVSCSDto result = conceptCodeServiceImpl.create();
+    }
+
+    @Test(expected = CodeSystemNotFoundException.class)
+    public void testCreateCodeSystem_throw_CodeSystemNotFoundException()
+            throws ValueSetNotFoundException,
+            CodeSystemVersionNotFoundException, CodeSystemNotFoundException {
+        ConceptCodeVSCSDto conceptCodeVSCSDto = mock(ConceptCodeVSCSDto.class);
+        List<ValueSet> valueSets = new ArrayList<ValueSet>();
+        ValueSet valueSet = mock(ValueSet.class);
+        valueSets.add(valueSet);
+        when(valueSetRepository.findAll()).thenReturn(valueSets);
+        when(codeSystemVersionRepository.findOne(anyLong())).thenReturn(null);
+        ConceptCodeVSCSDto result = conceptCodeServiceImpl.create();
+    }
+
+    @Test(expected = CodeSystemNotFoundException.class)
+    public void testCreateCodeSystemFromDto_throw_CodeSystemNotFoundException()
+            throws ValueSetNotFoundException, CodeSystemNotFoundException,
+            DuplicateConceptCodeException {
+        ConceptCodeDto created = mock(ConceptCodeDto.class);
+
+        when(created.getDescription()).thenReturn("description");
+        when(created.getCodeSystemVersionId()).thenReturn((long) 1);
+
+        when(codeSystemVersionRepository.findOne(anyLong())).thenReturn(null);
+        conceptCodeServiceImpl.create(created);
+    }
+
+    @Test(expected = ValueSetNotFoundException.class)
+    public void testCreateCodeSystemFromDto_throw_ValueSetNotFoundException()
+            throws ValueSetNotFoundException, CodeSystemNotFoundException,
+            DuplicateConceptCodeException {
+        ConceptCodeDto created = mock(ConceptCodeDto.class);
+
+        when(created.getDescription()).thenReturn("description");
+        when(created.getCodeSystemVersionId()).thenReturn((long) 1);
+
+        CodeSystemVersion selectedCsv = mock(CodeSystemVersion.class);
+        when(codeSystemVersionRepository.findOne(anyLong())).thenReturn(
+                selectedCsv);
+
+        List<Long> selVsIds = Arrays.asList((long) 1, (long) 2, (long) 3);
+
+        when(created.getValueSetIds()).thenReturn(null);
+
+        conceptCodeServiceImpl.create(created);
+    }
+
+    @Test(expected = ValueSetNotFoundException.class)
+    public void testCreateCodeSystemFromDto_throw_ValueSetNotFoundException_2()
+            throws ValueSetNotFoundException, CodeSystemNotFoundException,
+            DuplicateConceptCodeException {
+        ConceptCodeDto created = mock(ConceptCodeDto.class);
+
+        when(created.getDescription()).thenReturn("description");
+        when(created.getCodeSystemVersionId()).thenReturn((long) 1);
+
+        CodeSystemVersion selectedCsv = mock(CodeSystemVersion.class);
+        when(codeSystemVersionRepository.findOne(anyLong())).thenReturn(
+                selectedCsv);
+
+        List<Long> selVsIds = Arrays.asList((long) 1, (long) 2, (long) 3);
+        when(created.getValueSetIds()).thenReturn(selVsIds);
+
+        when(valueSetRepository.findOne(anyLong())).thenReturn(null);
+
+        conceptCodeServiceImpl.create(created);
+    }
+
+    @Test
+    public void testCreateCodeSystemFromDto_throw_ValueSetNotFoundException_3()
+            throws ValueSetNotFoundException, CodeSystemNotFoundException,
+            DuplicateConceptCodeException {
+        ConceptCodeDto created = mock(ConceptCodeDto.class);
+
+        when(created.getDescription()).thenReturn("description");
+        when(created.getCodeSystemVersionId()).thenReturn((long) 1);
+
+        CodeSystemVersion selectedCsv = mock(CodeSystemVersion.class);
+        when(codeSystemVersionRepository.findOne(anyLong())).thenReturn(
+                selectedCsv);
+
+        List<Long> selVsIds = Arrays.asList((long) 1, (long) 2, (long) 3);
+        when(created.getValueSetIds()).thenReturn(selVsIds);
+
+        ValueSet selectedVs = mock(ValueSet.class);
+
+        when(valueSetRepository.findOne(anyLong())).thenReturn(selectedVs);
+
+        when(created.getCode()).thenReturn("code");
+        when(selectedCsv.getId()).thenReturn((long) 1);
+
+        ConceptCode conceptCode = mock(ConceptCode.class);
+        when(
+                conceptCodeRepository.findByCodeAndCodeSystemVersionId(
+                        anyString(), anyLong())).thenReturn(conceptCode);
+
+        ConceptCodeDto conceptCodeDto = mock(ConceptCodeDto.class);
+        when(
+                valueSetMgmtHelper
+                        .createConceptCodeDtoFromEntity(any(ConceptCode.class)))
+                .thenReturn(conceptCodeDto);
+
+        assertEquals(conceptCodeServiceImpl.create(created), conceptCodeDto);
+    }
+
+    @Test
+    public void testCreateCodeSystemFromDto_3()
+            throws ValueSetNotFoundException, CodeSystemNotFoundException,
+            DuplicateConceptCodeException {
+        ConceptCodeDto created = mock(ConceptCodeDto.class);
+
+        when(created.getDescription()).thenReturn("description");
+        when(created.getCodeSystemVersionId()).thenReturn((long) 1);
+
+        CodeSystemVersion selectedCsv = mock(CodeSystemVersion.class);
+        when(codeSystemVersionRepository.findOne(anyLong())).thenReturn(
+                selectedCsv);
+
+        List<Long> selVsIds = Arrays.asList((long) 1, (long) 2, (long) 3);
+        when(created.getValueSetIds()).thenReturn(selVsIds);
+
+        ValueSet selectedVs = mock(ValueSet.class);
+
+        when(valueSetRepository.findOne(anyLong())).thenReturn(selectedVs);
+
+        when(created.getCode()).thenReturn("code");
+        when(selectedCsv.getId()).thenReturn((long) 1);
+
+        ConceptCode conceptCode = mock(ConceptCode.class);
+        when(
+                conceptCodeRepository.findByCodeAndCodeSystemVersionId(
+                        anyString(), anyLong())).thenReturn(null);
+
+        ConceptCodeDto conceptCodeDto = mock(ConceptCodeDto.class);
+        when(
+                valueSetMgmtHelper
+                        .createConceptCodeDtoFromEntity(any(ConceptCode.class)))
+                .thenReturn(conceptCodeDto);
+
+        assertEquals(conceptCodeServiceImpl.create(created), conceptCodeDto);
+    }
+
+}
