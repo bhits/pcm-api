26bde6ee79beaae0f392caec1a636176b4766e7c
burcakulug <burcak.ulug@feisystems.com>
2016-01-22 17:48:35 -0500

    Change PurposeOfUseCodeServiceImpl configuration to annotation based
    #7802:As a developer, I can apply UAA authorization to the PCM API to make the application secure

--- pcm-web/src/main/java/gov/samhsa/mhc/pcm/service/reference/PurposeOfUseCodeServiceImpl.java
+++ pcm-web/src/main/java/gov/samhsa/mhc/pcm/service/reference/PurposeOfUseCodeServiceImpl.java
@@ -1,6 +1,6 @@
 /*******************************************************************************
  * Open Behavioral Health Information Technology Architecture (OBHITA.org)
- * <p>
+ * <p/>
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
  * * Redistributions of source code must retain the above copyright
@@ -11,7 +11,7 @@
  * * Neither the name of the <organization> nor the
  * names of its contributors may be used to endorse or promote products
  * derived from this software without specific prior written permission.
- * <p>
+ * <p/>
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -28,6 +28,8 @@ package gov.samhsa.mhc.pcm.service.reference;
 import gov.samhsa.mhc.pcm.domain.reference.PurposeOfUseCode;
 import gov.samhsa.mhc.pcm.domain.reference.PurposeOfUseCodeRepository;
 import gov.samhsa.mhc.vss.service.dto.AddConsentFieldsDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
@@ -37,22 +39,12 @@ import java.util.List;
  * The Class PurposeOfUseCodeServiceImpl.
  */
 @Transactional
+@Service
 public class PurposeOfUseCodeServiceImpl implements PurposeOfUseCodeService {
 
     /** The purpose of use code repository. */
-    PurposeOfUseCodeRepository purposeOfUseCodeRepository;
-
-    /**
-     * Instantiates a new purpose of use code service impl.
-     *
-     * @param purposeOfUseCodeRepository
-     *            the purpose of use code repository
-     */
-    public PurposeOfUseCodeServiceImpl(
-            PurposeOfUseCodeRepository purposeOfUseCodeRepository) {
-        super();
-        this.purposeOfUseCodeRepository = purposeOfUseCodeRepository;
-    }
+    @Autowired
+    private PurposeOfUseCodeRepository purposeOfUseCodeRepository;
 
     /*
      * (non-Javadoc)
