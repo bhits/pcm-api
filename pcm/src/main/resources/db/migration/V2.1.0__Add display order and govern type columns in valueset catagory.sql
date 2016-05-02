29be8880074f265feca191813def2e9491e5859b
JiahaoLi <Jiahao.Li@feisystems.com>
2016-05-01 20:09:19 -0400

    Disable display order to pick up native value in valueset category
    #8796 - As a Patient, I am able to distinguish between categories that are driven by Federal and State jurisdictional privacy policy rules

--- pcm/src/main/resources/db/migration/V2.1.0__Add display order and govern type columns in valueset catagory.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE value_set_category  ADD COLUMN is_federal BOOLEAN;
-ALTER TABLE value_set_category  ADD COLUMN display_order BIGINT UNIQUE;
-ALTER TABLE value_set_category_aud  ADD COLUMN is_federal BOOLEAN;
-ALTER TABLE value_set_category_aud  ADD COLUMN display_order BIGINT UNIQUE;
