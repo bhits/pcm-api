eb353679dbcbc915579e066a5d506cf37cbab035
JiahaoLi <Jiahao.Li@feisystems.com>
2016-05-01 20:20:05 -0400

    Enable government type not be null value in valueset category
    #8796 - As a Patient, I am able to distinguish between categories that are driven by Federal and State jurisdictional privacy policy rules

--- pcm/src/main/resources/db/migration/V2.1.0__Add display order and govern type columns in valueset category.sql
+++ pcm/src/main/resources/db/migration/V2.1.0__Add display order and govern type columns in valueset category.sql
@@ -1,4 +1,4 @@
-ALTER TABLE value_set_category  ADD COLUMN is_federal BOOLEAN;
+ALTER TABLE value_set_category  ADD COLUMN is_federal BOOLEAN NOT NULL;
 ALTER TABLE value_set_category  ADD COLUMN display_order BIGINT UNSIGNED UNIQUE;
-ALTER TABLE value_set_category_aud  ADD COLUMN is_federal BOOLEAN;
+ALTER TABLE value_set_category_aud  ADD COLUMN is_federal BOOLEAN NOT NULL;
 ALTER TABLE value_set_category_aud  ADD COLUMN display_order BIGINT UNSIGNED UNIQUE;
