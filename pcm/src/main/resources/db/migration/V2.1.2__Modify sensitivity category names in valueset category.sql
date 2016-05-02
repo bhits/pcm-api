ce15f702ac7f16471ad2ac13cba34c11dabf53ff
JiahaoLi <Jiahao.Li@feisystems.com>
2016-05-02 12:58:08 -0400

    Modify sensitivity category names in valueset category
    #8796 - As a Patient, I am able to distinguish between categories that are driven by Federal and State jurisdictional privacy policy rules

---
+++ pcm/src/main/resources/db/migration/V2.1.2__Modify sensitivity category names in valueset category.sql
@@ -0,0 +1,8 @@
+UPDATE value_set_category SET name = 'Substance/Drug use information' WHERE valueset_cat_id = '1';
+UPDATE value_set_category SET name = 'Genetic disease information' WHERE valueset_cat_id = '2';
+UPDATE value_set_category SET name = 'HIV/AIDS information' WHERE valueset_cat_id = '3';
+UPDATE value_set_category SET name = 'Mental health information' WHERE valueset_cat_id = '4';
+UPDATE value_set_category SET name = 'Sexuality and reproductive health information' WHERE valueset_cat_id = '6';
+UPDATE value_set_category SET name = 'Alcohol use and Alcoholism Information' WHERE valueset_cat_id = '9';
+UPDATE value_set_category SET name = 'Communicable disease information' WHERE valueset_cat_id = '10';
+UPDATE value_set_category SET name = 'Addictions information' WHERE valueset_cat_id = '11';
