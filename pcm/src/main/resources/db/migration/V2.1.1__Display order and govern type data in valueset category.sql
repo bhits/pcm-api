d08b952776b728d5a57f437c837549eeddee6385
JiahaoLi <Jiahao.Li@feisystems.com>
2016-05-02 15:45:36 -0400

    Initial Valueset Category Display Order
    #8796 - As a Patient, I am able to distinguish between categories that are driven by Federal and State jurisdictional privacy policy rules

--- pcm/src/main/resources/db/migration/V2.1.1__Display order and govern type data in valueset category.sql
+++
@@ -1,8 +0,0 @@
-UPDATE value_set_category SET is_federal = TRUE WHERE valueset_cat_id = '1';
-UPDATE value_set_category SET is_federal = FALSE WHERE valueset_cat_id = '2';
-UPDATE value_set_category SET is_federal = FALSE WHERE valueset_cat_id = '3';
-UPDATE value_set_category SET is_federal = FALSE WHERE valueset_cat_id = '4';
-UPDATE value_set_category SET is_federal = FALSE WHERE valueset_cat_id = '6';
-UPDATE value_set_category SET is_federal = TRUE WHERE valueset_cat_id = '9';
-UPDATE value_set_category SET is_federal = FALSE WHERE valueset_cat_id = '10';
-UPDATE value_set_category SET is_federal = FALSE WHERE valueset_cat_id = '11';
--- pcm/src/main/resources/db/migration/V2.1.1__Modify sensitivity category names in valueset category.sql
+++ pcm/src/main/resources/db/migration/V2.1.1__Modify sensitivity category names in valueset category.sql
@@ -1,4 +1,4 @@
-UPDATE value_set_category SET name = 'Substance/Drug use information' WHERE valueset_cat_id = '1';
+UPDATE value_set_category SET name = 'Drug use information' WHERE valueset_cat_id = '1';
 UPDATE value_set_category SET name = 'Genetic disease information' WHERE valueset_cat_id = '2';
 UPDATE value_set_category SET name = 'HIV/AIDS information' WHERE valueset_cat_id = '3';
 UPDATE value_set_category SET name = 'Mental health information' WHERE valueset_cat_id = '4';
