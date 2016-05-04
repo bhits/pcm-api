6b9d8b019b3c259f12120ab3611d7e7cb6c6bcbb
JiahaoLi <Jiahao.Li@feisystems.com>
2016-05-04 16:32:07 -0400

    Modify sensitivity category descriptions in valueset category
    #8796 - As a Patient, I am able to distinguish between categories that are driven by Federal and State jurisdictional privacy policy rules

--- pcm/src/main/resources/db/migration/V2.1.1__Modify sensitivity category names in valueset category.sql
+++ pcm/src/main/resources/db/migration/V2.1.1__Modify sensitivity category names in valueset category.sql
@@ -1,8 +1,8 @@
-UPDATE value_set_category SET name = 'Drug use information' WHERE valueset_cat_id = '1';
-UPDATE value_set_category SET name = 'Genetic disease information' WHERE valueset_cat_id = '2';
-UPDATE value_set_category SET name = 'HIV/AIDS information' WHERE valueset_cat_id = '3';
-UPDATE value_set_category SET name = 'Mental health information' WHERE valueset_cat_id = '4';
-UPDATE value_set_category SET name = 'Sexuality and reproductive health information' WHERE valueset_cat_id = '6';
-UPDATE value_set_category SET name = 'Alcohol use and Alcoholism Information' WHERE valueset_cat_id = '9';
-UPDATE value_set_category SET name = 'Communicable disease information' WHERE valueset_cat_id = '10';
-UPDATE value_set_category SET name = 'Addictions information' WHERE valueset_cat_id = '11';
+UPDATE value_set_category SET name = 'Drug use information' WHERE code = 'ETH';
+UPDATE value_set_category SET name = 'Genetic disease information' WHERE code = 'GDIS';
+UPDATE value_set_category SET name = 'HIV/AIDS information' WHERE code = 'HIV';
+UPDATE value_set_category SET name = 'Mental health information' WHERE code = 'PSY';
+UPDATE value_set_category SET name = 'Sexuality and reproductive health information' WHERE code = 'SEX';
+UPDATE value_set_category SET name = 'Alcohol use and Alcoholism Information' WHERE code = 'ALC';
+UPDATE value_set_category SET name = 'Communicable disease information' WHERE code = 'COM';
+UPDATE value_set_category SET name = 'Addictions information' WHERE code = 'ADD';
