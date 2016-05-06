9f477cc3f003c59b6693ffe94936c5a5ef4ac900
JiahaoLi <Jiahao.Li@feisystems.com>
2016-05-05 21:28:03 -0400

    Combine the scripts for modifying  valueset category values
    #8796 - As a Patient, I am able to distinguish between categories that are driven by Federal and State jurisdictional privacy policy rules

--- pcm/src/main/resources/db/migration/V2.1.1__Modify sensitivity category names in valueset category.sql
+++
@@ -1,8 +0,0 @@
-UPDATE value_set_category SET name = 'Drug use information' WHERE code = 'ETH';
-UPDATE value_set_category SET name = 'Genetic disease information' WHERE code = 'GDIS';
-UPDATE value_set_category SET name = 'HIV/AIDS information' WHERE code = 'HIV';
-UPDATE value_set_category SET name = 'Mental health information' WHERE code = 'PSY';
-UPDATE value_set_category SET name = 'Sexuality and reproductive health information' WHERE code = 'SEX';
-UPDATE value_set_category SET name = 'Alcohol use and Alcoholism Information' WHERE code = 'ALC';
-UPDATE value_set_category SET name = 'Communicable disease information' WHERE code = 'COM';
-UPDATE value_set_category SET name = 'Addictions information' WHERE code = 'ADD';
