6e516c4b594286ee2a9f9d80be92b2931cbe406c
Michael Hadjiosif <michael.hadjiosif@feisystems.com>
2016-06-01 19:19:43 -0400

    Fix ConsentRevocationTermsVersions repository & service
    #9082 - As a patient, I will no longer be required to sign a consent and now can use a checkbox to complete my attestation for revoking a consent ( Part 2 Revocation)

--- pcm/src/main/java/gov/samhsa/mhc/pcm/service/consent/ConsentRevocationTermsVersionsServiceImpl.java
+++ pcm/src/main/java/gov/samhsa/mhc/pcm/service/consent/ConsentRevocationTermsVersionsServiceImpl.java
@@ -3,9 +3,12 @@ package gov.samhsa.mhc.pcm.service.consent;
 import gov.samhsa.mhc.pcm.domain.consent.ConsentRevocationTermsVersions;
 import gov.samhsa.mhc.pcm.domain.consent.ConsentRevocationTermsVersionsRepository;
 import gov.samhsa.mhc.pcm.service.dto.ConsentRevocationTermsVersionsDto;
+import gov.samhsa.mhc.pcm.service.exception.ConsentRevocationTermsVersionNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Transactional
 @Service
@@ -14,21 +17,49 @@ public class ConsentRevocationTermsVersionsServiceImpl implements ConsentRevocat
     @Autowired
     private ConsentRevocationTermsVersionsRepository consentRevocationTermsVersionsRepository;
 
+
+    /**
+     * Search for the latest enabled version.
+     *
+     * @return ConsentRevocationTermsVersions
+     */
     @Override
     public ConsentRevocationTermsVersions findByLatestEnabledVersion() {
-        return consentRevocationTermsVersionsRepository.findOneByVersionDisabled(false);
+        List<ConsentRevocationTermsVersions> consentRevocationTermsVersionsList = consentRevocationTermsVersionsRepository.findAllByVersionDisabledOrderByAddedDateTimeDesc(false);
+        ConsentRevocationTermsVersions consentRevocationTermsVersions;
+
+        try{
+            consentRevocationTermsVersions = consentRevocationTermsVersionsList.get(0);
+        }catch(IndexOutOfBoundsException e){
+            consentRevocationTermsVersions = null;
+        }
+
+        return consentRevocationTermsVersions;
     }
 
+    /**
+     * Search for the latest enabled version and return
+     * the DTO for that version.
+     *
+     * @return ConsentRevocationTermsVersionsDto
+     */
     @Override
     public ConsentRevocationTermsVersionsDto findDtoByLatestEnabledVersion() {
+        ConsentRevocationTermsVersionsDto consentRevocationTermsVersionsDto;
+
         ConsentRevocationTermsVersions consentRevocationTermsVersions = findByLatestEnabledVersion();
 
-        ConsentRevocationTermsVersionsDto consentRevocationTermsVersionsDto = new ConsentRevocationTermsVersionsDto();
+        if (consentRevocationTermsVersions == null){
+            throw new ConsentRevocationTermsVersionNotFoundException("No active ConsentRevocationTermsVersions record found in database");
+        }else {
 
-        consentRevocationTermsVersionsDto.setId(consentRevocationTermsVersions.getId());
-        consentRevocationTermsVersionsDto.setAddedDateTime(consentRevocationTermsVersions.getAddedDateTime());
-        consentRevocationTermsVersionsDto.setConsentRevokeTermsText(consentRevocationTermsVersions.getConsentRevokeTermsText());
-        consentRevocationTermsVersionsDto.setVersionDisabled(consentRevocationTermsVersions.getVersionDisabled());
+            consentRevocationTermsVersionsDto = new ConsentRevocationTermsVersionsDto();
+
+            consentRevocationTermsVersionsDto.setId(consentRevocationTermsVersions.getId());
+            consentRevocationTermsVersionsDto.setAddedDateTime(consentRevocationTermsVersions.getAddedDateTime());
+            consentRevocationTermsVersionsDto.setConsentRevokeTermsText(consentRevocationTermsVersions.getConsentRevokeTermsText());
+            consentRevocationTermsVersionsDto.setVersionDisabled(consentRevocationTermsVersions.getVersionDisabled());
+        }
 
         return consentRevocationTermsVersionsDto;
     }
---
+++ pcm/src/main/java/gov/samhsa/mhc/pcm/service/exception/ConsentRevocationTermsVersionNotFoundException.java
@@ -0,0 +1,9 @@
+package gov.samhsa.mhc.pcm.service.exception;
+
+public class ConsentRevocationTermsVersionNotFoundException extends RuntimeException {
+
+    public ConsentRevocationTermsVersionNotFoundException(String message) {
+        super(message);
+    }
+
+}
