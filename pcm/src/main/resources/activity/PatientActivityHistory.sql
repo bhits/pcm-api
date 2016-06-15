21f27f32c6f745f02a345513c345b8f512bf802f
TomsonNgassa <Tomson.Ngassa@feisystems.com>
2016-06-15 09:18:06 -0400

    Add patient activity history sql.
    #8795 - As a patient, for signing a consent, I will be able to use a checkbox attestation feature

---
+++ pcm/src/main/resources/activity/PatientActivityHistory.sql
@@ -0,0 +1,17 @@
+SELECT
+  m.id AS activityId,
+  r.rev AS revisionid,
+  r.revtstmp AS timestamp,
+  r.username AS changedBy,
+  pa.first_name AS firstName,
+  pa.last_name AS lastName,
+  rtc.display_name AS recType,
+  m.entity_class_name AS type
+FROM
+  revinfo r
+    LEFT JOIN
+  modified_entity_type_entity m ON r.rev = m.revision
+    LEFT JOIN
+  revision_type_code rtc ON m.revision_type = rtc.code
+    LEFT JOIN
+  patient_audit pa ON r.rev = pa.rev
