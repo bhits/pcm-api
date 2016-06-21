b0e31598573905559863bee4b6fed7c98ce2b1cc
JiahaoLi <Jiahao.Li@feisystems.com>
2016-06-21 16:22:12 -0400

    Remove patient changing action in activity history
    #9174 - As a patient, I will have access to my activity history, so that I can view my activity trail within the system

--- pcm/src/main/resources/activity/PatientActivityHistory.sql
+++ pcm/src/main/resources/activity/PatientActivityHistory.sql
@@ -6,9 +6,18 @@ SELECT
   rtc.display_name AS rec_type,
   m.entity_class_name AS type
 FROM
-  revinfo AS r
+(
+  select *
+  from modified_entity_type_entity m
+  where
+    case
+      when m.entity_class_name = "gov.samhsa.mhc.pcm.domain.patient.Patient"
+      then m.revision_type <> 1
+      else true
+    end
+) as m
     LEFT JOIN
-  modified_entity_type_entity m ON r.rev = m.revision
+  revinfo AS r ON r.rev = m.revision
     LEFT JOIN
   revision_type_code rtc ON m.revision_type = rtc.code
     LEFT JOIN
