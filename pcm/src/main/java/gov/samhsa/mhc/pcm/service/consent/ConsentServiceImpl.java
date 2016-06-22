970a5cd698d57c4a7f3cdca40f31cd202d0b78d5
Michael Hadjiosif <michael.hadjiosif@feisystems.com>
2016-06-21 22:16:06 -0400

    Add DB migration 5.1.0 to make attester_middle_name field optional
    #9219 - As a provider, I will be able to utilize a feature to audit a patient consent when signed or revoked

--- pcm/src/main/java/gov/samhsa/mhc/pcm/service/consent/ConsentServiceImpl.java
+++ pcm/src/main/java/gov/samhsa/mhc/pcm/service/consent/ConsentServiceImpl.java
@@ -927,7 +927,7 @@ public class ConsentServiceImpl implements ConsentService {
             attestedConsent.setAttesterEmail(patient.getEmail());
             attestedConsent.setAttesterLastName(patient.getLastName());
             // Middle name not available in Patient entity to be updated
-            attestedConsent.setAttesterMiddleName("");
+            attestedConsent.setAttesterMiddleName(null);
             attestedConsent.setAttesterFirstName(patient.getFirstName());
 
             Date attestedOn = new Date();
@@ -965,7 +965,7 @@ public class ConsentServiceImpl implements ConsentService {
             attestedConsentRevocation.setAttesterEmail(patient.getEmail());
             attestedConsentRevocation.setAttesterLastName(patient.getLastName());
             // Middle name not available in Patient entity to be updated
-            attestedConsentRevocation.setAttesterMiddleName("");
+            attestedConsentRevocation.setAttesterMiddleName(null);
             attestedConsentRevocation.setAttesterFirstName(patient.getFirstName());
 
             Date revokedOn = new Date();
